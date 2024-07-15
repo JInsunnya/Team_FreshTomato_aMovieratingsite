b9f1fee6f02d40f42e22940c89e605c1215845ee
LYZAcell <applelgy@naver.com>
2024-07-15 15:13:23 +0900

    json 10개 슬라이싱 성공

--- src/component/MovieList.jsx
+++ src/component/MovieList.jsx
@@ -11,7 +11,10 @@ const MovieList = () => {
       try {
         const response = await axios.get('https://port-0-minihackathon-12-lyec0qpi97716ac6.sel5.cloudtype.app/movie/list');
         // API에서 가져온 영화 데이터를 변수에 저장
-        const movieData = response.data;
+        let movieData = response.data;
+
+        // 처음 10개의 영화 데이터만 추출
+        movieData = movieData.slice(0, 10);
 
         setMovies(movieData); // 전체 영화 데이터를 설정
         setLoading(false);
@@ -37,27 +40,17 @@ const MovieList = () => {
     return <div>No movies found.</div>;
   }
 
-  // 영화 데이터를 5개씩 묶어서 행(row)으로 만들기
-  const moviesInRows = [];
-  for (let i = 0; i < movies.length; i += 5) {
-    const row = movies.slice(i, i + 5);
-    moviesInRows.push(row);
-  }
-
   return (
     <div>
       <h1>Movie List</h1>
-      {moviesInRows.map((row, index) => (
-        <ul key={index} style={{ display: 'flex', listStyle: 'none', padding: 0 }}>
-          {row.map(movie => (
-            <li key={movie.id} style={{ margin: '0.5rem' }}>
-              <h2>{movie.title_kor}</h2>
-              {/*이미지 로딩 주석처리*/}
-              {/*<img src={movie.poster_url} alt={movie.title_kor} style={{ maxWidth: '70%' }} />*/}
-            </li>
-          ))}
-        </ul>
-      ))}
+      <ul style={{ display: 'flex', listStyle: 'none', padding: 0 }}>
+        {movies.map(movie => (
+          <li key={movie.id} style={{ margin: '0.5rem' }}>
+            <h2>{movie.title_kor}</h2>
+            <img src={movie.poster_url} alt={movie.title_kor} style={{ maxWidth: '100%' }} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
