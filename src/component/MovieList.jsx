81c9e852449c9191071553fe8ffa45adc5a3c175
LYZAcell <applelgy@naver.com>
2024-07-16 09:31:16 +0900

    진선 충돌 해결(MovieList) 및 detail 해결

--- src/component/MovieList.jsx
+++ src/component/MovieList.jsx
@@ -2,13 +2,11 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
-
 const MovieListContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
 `;
-
 const MovieItem = styled.div`
   margin: 10px;
   padding: 10px;
@@ -17,17 +15,14 @@ const MovieItem = styled.div`
   width: 200px;
   text-align: center;
 `;
-
 const MovieTitle = styled.h2`
   font-size: 1.2rem;
   margin-bottom: 10px;
 `;
-
 const MovieImage = styled.img`
   max-width: 100%;
   height: auto;
 `;
-
 const MovieList = () => {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -35,16 +30,12 @@ const MovieList = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [perPage] = useState(20); // 한 페이지에 보여질 항목 수
   const navigate = useNavigate();
-
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(
-          'https://port-0-minihackathon-12-lyec0qpi97716ac6.sel5.cloudtype.app/movie/list'
-        );
-        let movieData = response.data;
-        movieData = movieData.slice(0, 20);
-        setMovies(movieData);
+        setLoading(true);
+        const response = await axios.get(`https://freshtomato.store/Movie/home/${currentPage}/`);
+        setMovies(response.data);
         setLoading(false);
       } catch (error) {
         console.error('에러 발생:', error);
@@ -52,36 +43,28 @@ const MovieList = () => {
         setLoading(false);
       }
     };
-
     fetchData();
   }, [currentPage]);
-
   const handleNextPage = () => {
     setCurrentPage(currentPage + 1);
   };
-
   const handlePrevPage = () => {
     if (currentPage > 1) {
       setCurrentPage(currentPage - 1);
     }
   };
-
   const handleClick = (id) => {
     navigate(`/detail/${id}`);
   };
-
   if (loading) {
     return <div>Loading...</div>;
   }
-
   if (error) {
     return <div>에러 발생: {error.message}</div>;
   }
-
   if (movies.length === 0) {
     return <div>No movies found.</div>;
   }
-
   return (
     <div>
       <MovieListContainer>
@@ -93,13 +76,10 @@ const MovieList = () => {
         ))}
       </MovieListContainer>
       <div>
-        <button onClick={handlePrevPage} disabled={currentPage === 1}>
-          이전 페이지
-        </button>
+        <button onClick={handlePrevPage} disabled={currentPage === 1}>이전 페이지</button>
         <button onClick={handleNextPage}>다음 페이지</button>
       </div>
     </div>
   );
 };
-
 export default MovieList;
