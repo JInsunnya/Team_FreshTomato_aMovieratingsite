6a3986b24b2c27e37cd182c930e411939bee0fa7
Chaeri-1 <alfsdo03@husf.ac.kr>
2024-07-16 10:49:54 +0900

    register success

--- src/user/register.jsx
+++ src/user/register.jsx
@@ -1,6 +1,9 @@
 import React, {useState} from "react";
 import { RegisterSpace, RegisterInput } from "./styles-cr";
 import axios from 'axios';
+//import { Cookies } from "react-cookie";
+
+//const [cookies, setCookies, removeCookie] = useCookies();
 
 export default function Register() {
   const [username, setUsername] = useState("")
@@ -13,26 +16,26 @@ export default function Register() {
       'https://freshtomato.store/',
   });
   
-  const register = async () => {
+  const handleregister = async () => {
     const data = {
       username: username,
       password1: password1,
       password2: password2,
       nickname: nickname,
-    }
+    };
+    console.log(data);
     try {
-      const response = await api.post("/dj/registration/", data)
+      const response = await api.post("dj/registration/", data)
       console.log('회원가입 성공', response.data)
       setUsername("")
       setPassword1("")
       setPassword2("")
       setNickname("")
       localStorage.setItem("access", response.data.access);
-      return response.data;
     } catch (error) {
       return error;
-    }
-  }
+    };
+  };
 
   return(
     <div className="register-page">
@@ -42,27 +45,35 @@ export default function Register() {
           <p>아이디</p>
           <input
             type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
           />
         </RegisterInput>
         <RegisterInput>
           <p>비밀번호</p>
           <input
             type="password"
+            value={password1}
+            onChange={(e) => setPassword1(e.target.value)}
           />
         </RegisterInput>
         <RegisterInput>
           <p>비밀번호 확인</p>
           <input
             type="password"
+            value={password2}
+            onChange={(e) => setPassword2(e.target.value)}
           />
         </RegisterInput>
         <RegisterInput>
           <p>이름(닉네임)</p>
           <input
             type="text"
+            value={nickname}
+            onChange={(e) => setNickname(e.target.value)}
           />
         </RegisterInput>
-        <button onClick={register}>회원가입하기</button>
+        <button onClick={handleregister}>회원가입하기</button>
       </RegisterSpace>
     </div>
   )
